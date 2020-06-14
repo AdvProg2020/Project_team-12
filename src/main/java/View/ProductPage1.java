4ab35ca36cca37a2a3d34268760c798580eedbaa
ali.njb <njibiali@gmail.com>
2020-06-14 11:58:33 +0430

    some changes.

---
+++ src/main/java/View/ProductPage1.java
@@ -0,0 +1,4 @@
+package View;
+
+public class ProductPage1 {
+}
--- src/main/java/View/SetupPage1.java
+++ src/main/java/View/SetupPage1.java
@@ -3,15 +3,28 @@ package View;
 
 import com.jfoenix.controls.JFXHamburger;
 import com.jfoenix.transitions.hamburger.HamburgerSlideCloseTransition;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Bounds;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 import sun.swing.WindowsPlacesBar;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -21,7 +34,10 @@ public class SetupPage1 implements Initializable {
     boolean formIsOpen = false;
     @FXML
     private JFXHamburger test;
-
+    @FXML
+    ImageView imageView;
+    @FXML
+    ImageView profileImage;
     public SetupPage1() {
 
     }
@@ -81,6 +97,19 @@ public class SetupPage1 implements Initializable {
     private void proceed(){
 
     }
+
+    /*public void zoom(MouseEvent mouseEvent) {
+                cropImage((int)mouseEvent.getSceneX(),(int)mouseEvent.getSceneY(),imageView);
+    }
+    private void cropImage(int width,int height, ImageView imageView) {
+            SnapshotParameters parameters = new SnapshotParameters();
+            parameters.setFill(Color.TRANSPARENT);
+            parameters.setViewport(new Rectangle2D(width-10, height-10, width, height));
+
+            WritableImage wi = new WritableImage(50, 50);
+            Image croppedImage = imageView.snapshot(parameters, wi);
+            profileImage.setImage(croppedImage);
+    }*/
     class Filter extends FileFilter {
 
         public boolean accept(File f) {
@@ -96,6 +125,8 @@ public class SetupPage1 implements Initializable {
             return "(*.jpg),(*.png)";
         }
     }
+
+
 }
 //TODO:register and proceed method
 //TODO:animation for proceed button
